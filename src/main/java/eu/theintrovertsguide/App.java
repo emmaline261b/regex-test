64f70e6ebd98d3e6c23965302b2f1319947f7aac
Emmaline261 <emmaline261@gmail.com>
2022-08-05 17:14:34 +0200

    minor changes

--- src/main/java/eu/theintrovertsguide/App.java
+++ src/main/java/eu/theintrovertsguide/App.java
@@ -1,4 +1,8 @@
 package eu.theintrovertsguide;
 
 public class App {
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/main/java/eu/theintrovertsguide/LoginProblem.java
@@ -0,0 +1,22 @@
+package eu.theintrovertsguide;
+
+import java.util.Scanner;
+
+class LoginProblem {
+
+    public static void main(String[] args) {
+
+        /* The scanner object to read data from the standard input */
+        Scanner scanner = new Scanner(System.in);
+
+        /* The common pattern for valid logins */
+        String loginRegex = "\\s*[a-zA-Z0-9_$]{5,12}\\s*";
+
+        /* The read string which may be a login */
+        String mayBeLogin = scanner.nextLine();
+
+        boolean isLogin = mayBeLogin.matches(loginRegex);
+
+        System.out.println(isLogin);
+    }
+}
